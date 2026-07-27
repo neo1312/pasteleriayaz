866d92fcfc0f72d51ee5d31709dcd4ca757c0752
leonvic13 <leonvic13@gmail.com>
2026-07-27 16:44:49 -0600

    fix: match entrypoint env vars to .env.prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 set -e
+until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
 
-until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
     echo "Waiting for PostgreSQL..."
     sleep 2
 done
